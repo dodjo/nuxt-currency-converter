b8e8559687c8afedf2dc9209e05e5a557f1e8324
Vladimir Chmyr <vladimir.chmyr@gmail.com>
2025-05-07 10:52:37 +0300

    add animation pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
 <Header />
   <div class="my-5">
-    <NuxtPage />
+    <Transition name="page" mode="out-in">
+      <NuxtPage />
+    </Transition>
   </div>
 </template>
 
@@ -13,5 +15,13 @@ currencyStore.fetchRates()
 
 
 <style scoped>
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.3s ease;
+}
 
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+}
 </style>
